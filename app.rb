16260cd6f62d527ce6277917681668558dcf8147
danny.chen@tadashishoji.com <aznlucidx@gmail.com>
2013-08-07 14:15:26 -0700

    vote fix

--- app.rb
+++ app.rb
@@ -223,6 +223,19 @@ post '/email/create' do
 end
 
 get '/vote' do
+  @graph  = Koala::Facebook::API.new(access_token)
+
+  # Get public details of current application
+  @app  =  @graph.get_object(ENV["FACEBOOK_APP_ID"])
+
+  if access_token
+    @user    = @graph.get_object("me")
+    @friends = @graph.get_connections('me', 'friends')
+    @photos  = @graph.get_connections('me', 'photos')
+    @likes   = @graph.get_connections('me', 'likes').first(4)
+
+    # for other data you can always run fql
+    @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
   @groups = Group.first(:is_active => true)
   erb :"vote"
 end
