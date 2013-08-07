b263db5df8a6604c19fe0c16d0d203964643d815
danny.chen@tadashishoji.com <aznlucidx@gmail.com>
2013-08-07 14:31:56 -0700

    wtf

--- app.rb
+++ app.rb
@@ -178,7 +178,6 @@ get "/" do
     @photos  = @graph.get_connections('me', 'photos')
     @likes   = @graph.get_connections('me', 'likes').first(4)
     set :user_name, @user['me']['username']
-    set :email, @user['me']['email']
 
     # for other data you can always run fql
     @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
