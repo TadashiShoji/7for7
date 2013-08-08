7abb96a5170c3187adcbf46f67481cc24118bfc1
danny.chen@tadashishoji.com <aznlucidx@gmail.com>
2013-08-07 17:39:03 -0700

    fix

--- app.rb
+++ app.rb
@@ -185,9 +185,7 @@ get "/" do
     @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
   end
 
-  if Group.all(:votes => { :username => @user['username'] })
-    erb :novote
-  end
+  @wtf = Group.all(:votes => { :username => @user['username']})
 
   if @groups = Group.first(:is_active => true)
   erb :index
